4eaa903d394f40e46c1c3666de12b66262478c6e
David <davidwatsonbr@gmail.com>
2025-07-15 10:57:57 +0000

    subindo exercicio try catch - divisao por zero

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,14 @@
+public class Numero {
+    public static void dividir(int a, int b) {
+        int resultado = 0;
+        try {
+            resultado = a / b;
+        }
+        catch(ArithmeticException e) {
+            System.out.println("Nao eh possivel dividir por zero");
+        }
+        finally {
+            System.out.println(String.format("%d / %d = %d", a, b, resultado));
+        }
+    }
+}
