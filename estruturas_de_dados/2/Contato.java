3f6933ec0490759d28f7cfe556f300b381ca9d7d
David Watson <davidwatson@Davids-MacBook-Pro.local>
2025-07-21 19:28:57 -0300

    subindo exercicio final de estrutura de dados

--- estruturas_de_dados/2/Contato.java
+++ estruturas_de_dados/2/Contato.java
@@ -1,3 +1,29 @@
-public class Contato {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+public class Contato {
+    private List<String> telefones;
+    private Set<String> emails;
+
+    public Contato() {
+        telefones = new ArrayList<String>();
+        emails = new HashSet<String>();
+    }
+    public List<String> getTelefones() {
+        return telefones;
+    }
+
+    public void adicionarTelefone(String telefone) {
+        telefones.add(telefone);
+    }
+
+    public Set<String> getEmails() {
+        return emails;
+    }
+
+    public void adicionarEmail(String email) {
+        emails.add(email);
+    }
 }
