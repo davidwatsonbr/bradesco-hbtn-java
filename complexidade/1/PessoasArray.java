0453928ca4095548b3236b2503ca36ac272c483d
David <davidwatsonbr@gmail.com>
2025-07-22 12:22:26 +0000

    removendo linhas para o erro ficar na linha 32

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -23,13 +23,11 @@ public class PessoasArray {
         System.out.println(String.format("Procurando pelo nome: \"%s\"", nome));
         for (int i = 0; i < getNomes().length; i++) {
             System.out.println(String.format("Passando pelo indice:%d", i));
-
             if (nomes[i] == nome) {
                 encontrado = true;
                 System.out.println(String.format("Nome pesquisado é %s que está na posição %d", nome, i));
             }
         }
-
         if (!encontrado) {
             throw new IllegalArgumentException(String.format("O nome %s não se encontra no array de nomes", nome));
         }
