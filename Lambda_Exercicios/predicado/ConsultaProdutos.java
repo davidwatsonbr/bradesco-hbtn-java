d1f09efb63866ce9df105c62b7a6a3c52fc4bfd4
David Watson <davidwatson@Davids-MacBook-Pro.local>
2025-07-31 16:20:55 -0300

    subindo exercicio lambda - predicado

---
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ConsultaProdutos {
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> predicadoProduto) {
+        List<Produto> produtosFiltrados = new ArrayList<>();
+        for (Produto produto : produtos) {
+            if (predicadoProduto.test(produto)) {
+                produtosFiltrados.add(produto);
+            }
+        }
+        return produtosFiltrados;
+    }
+}
