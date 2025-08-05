207ae8876d1c8ba0cca8b676824bc957e2aa8c57
David <davidwatsonbr@gmail.com>
2025-08-05 17:16:17 +0000

    subindo exercicio collectors - 3

---
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/Pessoa.java
@@ -0,0 +1,32 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getNome() { return nome; }
+    public String getCargo() { return cargo; }
+    public int getIdade() { return idade; }
+    public double getSalario() { return salario; }
+
+    @Override
+    public String toString() {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("pt", "BR"));
+        DecimalFormat df = new DecimalFormat("#0.000000", symbols);
+        String salarioFormatado = df.format(salario);
+        return String.format("[%d] %s %s %d R$ %s", codigo, nome, cargo, idade, salarioFormatado);
+    }
+}
