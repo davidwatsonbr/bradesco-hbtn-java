023ed6f6698715d05faa71413f7316834320dddf
David <davidwatsonbr@gmail.com>
2025-08-05 14:31:38 +0000

    subindo exercicio streams collector - 0

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream()
+            .collect(Collectors.toCollection(TreeMap::new),
+                     Collectors.groupingBy(Pessoa::getCargo, Collectors.toCollection(TreeSet::new));
+    }
+}
