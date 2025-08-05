da09896d5e79f5ca6ca12db9e53ede380ea7318f
David <davidwatsonbr@gmail.com>
2025-08-05 17:48:33 +0000

    ajustando logica metodo obter hobbies

--- Streams_Collector_Optional_Exercicios/hobbies_por_cargo/ConsultaPessoas.java
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/ConsultaPessoas.java
@@ -34,11 +34,11 @@ public class ConsultaPessoas {
 
     public static Map<String, TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> pessoas) {
         return pessoas.stream()
-            .collect(Collectors.groupingBy(
-                    Pessoa::getCargo,
-                    Collectors.flatMapping(hobbies -> hobbies.stream())
-                    .toList()
-                )
-            );
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        Collectors.flatMapping(
+                                pessoa -> pessoa.getHobbies().stream(), Collectors.toCollection(TreeSet::new))
+                        )
+                );
     }
 }
