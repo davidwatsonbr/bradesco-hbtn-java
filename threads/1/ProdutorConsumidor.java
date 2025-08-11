40216f21c66175f77c6ec3b7eb6731612f311f5b
David <davidwatsonbr@gmail.com>
2025-08-11 11:53:32 +0000

    subindo exercicio threads - 1

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,19 @@
+public class ProdutorConsumidor {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+        // Criar e iniciar as threads do produtor e consumidor
+        Produtor produtor = new Produtor(buffer);
+        Consumidor consumidor = new Consumidor(buffer);
+
+        produtor.start();
+        consumidor.start();
+
+        try {
+            produtor.join();
+            consumidor.join();
+        }
+        catch(InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
