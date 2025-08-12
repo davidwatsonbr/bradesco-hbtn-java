aebb2490ac24b6cf387948cf684b3a16a47688e7
David Watson <davidwatson@Davids-MacBook-Pro.local>
2025-08-11 21:12:43 -0300

    tent erro

--- threads/2/Produtor.java
+++ threads/2/Produtor.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class Produtor extends Thread {
     private Fila fila;
+    private Random random = new Random();
 
     public Produtor(Fila fila) {
         this.fila = fila;
@@ -11,7 +12,7 @@ public class Produtor extends Thread {
     public void run() {
         try {
             while (true) {
-                int item = new Random().nextInt(100) + 1;
+                int item = random.nextInt(100) + 1;
                 fila.adicionar(item);
                 System.out.println(Thread.currentThread().getName() + " processando: " + item);
                 Thread.sleep(1000);
